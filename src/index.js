88bf8292e51843d024eab69f5fb679afbfb65eab
Daniel Parsley <danielparsley92@gmail.com>
2021-04-14 16:59:28 -0400

    Third draft after lecture

--- src/index.js
+++ src/index.js
@@ -14,29 +14,130 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-let allToysDiv = document.querySelector("div#toy-collection")
+/*let allToysDiv = document.querySelector("div#toy-collection")
+
 
 fetch("http://localhost:3000/toys")
    .then(res => res.json())
-   .then( function (toysArr){
-     toysArr.forEach(function(toyObj){
+   .then((toysArr) => {
+     toysArr.forEach(function(toysObj){
+       
+      //toysArr.forEach((toysObj) => {
+        //renderToyCard(toysObj)
+      })
 
-      turnToyToCard(toyObj)
+       let outerH2 = document.createElement(`h2`)
+           outerH2.className = `name`
+
+       let cardNameP = document.createElement(`p`)
+          cardNameP.className = `likes`
+
+        let cardImg = document.createElement(`img`)
+            cardImg.src = toysObj.image
+            cardImg.class = `toy-avatar`
+
+        let likeButton = document.createElement(`button`)
+            likeButton.class = `like-btn`
+            likeButton.innerText = `Like`
+
+            cardNameP.append(toysObj.name)
+            outerH2.append(cardNameP, likeButton)
+            allToysDiv.append(outerH2, cardImg)
      })
-   })
+   })*/
+
+
+   //HELPER FUNCTION
+   //Declare this globaly^^:
+let toyCollectionDiv = document.querySelector(`#toy-collection`)
+let toyForm = document.querySelector("form.add-toy-form")
 
 
 
-function turnToyToCard(aToy){
-   let aToyCard = document.createElement("div")
-       aToyCard.className = ("card")
 
-   let toyName = document.createElement("h2")
-       toyName.innerText = toy.name 
 
-    let toyImg = document.createElement("img")
+  function renderToyCard(toysObj){
+    let toyCardDiv = document.createElement(`div`)
+        toyCardDiv.className = `card`
+
+    let toyNameH2 = document.createElement(`h2`)
+        toyNameH2.innerText = toy.name
+
+    let toyImg = document.createElement(`img`)
         toyImg.src = toy.image
+        toyImg.className = `toy-avatar`
 
-    toyImg.className = ("toy-avatar")
-    toyImg.alt = toy.name
-}
+    let toyLikesP = document.createElement(`p`)
+        toyLikesP.innerText =   `${toy.like} Likes`
+
+    let likeButton = document.createElement(`button`)
+        likeButton.innerText = `Like <3`
+        likeButton.classList.add(`like-btn`)
+
+    //CONSTRUCT THE CARD
+    toyCardDiv.append(toyNameH2, toyImg, toyLikesP, likeButton)
+    
+    //APPEND TO THE DOM
+    toyCollectionDiv.append(toyCardDiv)
+
+
+    likeButton.addEventListener(`click`, (event) => {
+      
+       fetch(`http://localhost:3000/toys/${toy.id}`,{// Making a request to a specific id
+         method: "PATCH",
+         headers: {
+           "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+           likes: toy.likes + 1
+         })
+       })
+       .then(res => res.json())
+       .then((updatedToy) => {
+         //UPDATE DOM
+         toyLikesP.innerText = `${updatedToy.likes} Likes`
+
+         //UPDATE OBJECT IN MEMORY
+         toy.likes = updatedToy.likes
+       })
+    })
+
+
+  }
+
+
+
+  toyForm.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    // event -> form -> input -> value: this is how we get user input
+    // form -> input -> value: if we already have the event
+    //e.target will always be the form. Always use name or ID to get the user input
+    //e.target.name.value
+
+    let nameVar = e.target.name.value
+    let imageVar = e.target.image.value
+    fetch(`http://localhost:3000/toys`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: nameVar,
+        image: imageVar,
+        likes: 100
+      })
+    })
+    .then(res => res.json())
+    .then((newToy) => {
+      //Calling helper function to turn into HTML
+      renderToyCard(newToy)
+    })
+  })
+
+
+  //SAVING FETCH TO A LOCAL VARIABLE
+  function makingFetchHappen(url, configObj) {
+    return fetch(url, configObj).then(res => res.json())
+  }
+
+  //Promsied object that contains fufilled promise, actual object.
