073355b1c8a972530a0df2854ab965e2284abfbd
Jede <brahmajede@gmail.com>
2021-04-14 14:20:19 -0400

    kinda done?

--- src/index.js
+++ src/index.js
@@ -26,17 +26,74 @@ fetch("http://localhost:3000/toys")
    })
 
 
-
-function turnToyToCard(aToy){
+function turnToyToCard(toy){
    let aToyCard = document.createElement("div")
        aToyCard.className = ("card")
 
-   let toyName = document.createElement("h2")
-       toyName.innerText = toy.name 
+   let aToyName = document.createElement("h2")
+       aToyName.innerText = toy.name 
 
-    let toyImg = document.createElement("img")
-        toyImg.src = toy.image
+    let aToyImg = document.createElement("img")
+        aToyImg.src = toy.image
 
-    toyImg.className = ("toy-avatar")
-    toyImg.alt = toy.name
+        aToyImg.className = ("toy-avatar")
+        aToyImg.alt = toy.name
+    
+    let aToyLikeP = document.createElement("p")
+      aToyLikeP.innertext = `${toy.likes} Likes`
+    
+    let incrementButton = document.createElement("toy_id")
+      incrementButton.className = ("like-btn")
+      incrementButton.innerText = ("like")
+
+    aToyCard.append(aToyName, aToyImg, aToyLikeP, incrementButton)
+    allToysDiv.append(aToyCard)
+
+    incrementButton.addEventListener("click", function(event){
+//PATCH
+        fetch(`http://localhost:3000/toys/${toy.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({
+            likes: toy.likes +1
+          })
+        })
+        .then(res => res.json())
+        then(function (updatedtoyObj){
+          toy.likes = updatedtoyObj
+          aToyLikeP.innertext = `${updatedtoyObj.likes} Likes`
+        })
+      })
+
 }
+
+let Form = document.querySelector(".add-toy-form")
+Form.addEventListener('submit', function(e){
+  e.preventDefault()
+  let newToy = e.target.name.value
+  let newToyImg = e.target.image.value
+  
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  
+  body: JSON.stringify({
+    name: newToy,
+    image: newToyImage,
+    likes:0
+  })
+  })
+.then(res => res.json())
+.then(function(newToy){
+  turnToyToCard(newToy)
+})
+})
+
+
+
+
+//POST
